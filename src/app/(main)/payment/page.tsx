8477775f70d66f21b34964d4ed650a869ae1f103
PamanAleph <alieffirmandabisnis@gmail.com>
2024-04-28 17:10:16 +0700

    add order

--- src/app/(main)/payment/page.tsx
+++ src/app/(main)/payment/page.tsx
@@ -1,9 +1,21 @@
 import PaymentRadio from "@/components/payment/PaymentRadio";
+import Link from "next/link";
 import React from "react";
+import { MdOutlineArrowBack } from "react-icons/md";
 
 function page() {
   return (
     <section className="max-w-xl mx-auto px-2">
+      <div className="flex justify-between p-2 px-4 items-center">
+        <h1 className="text-[#262626] font-[700] text-[36px]">My Ticket</h1>
+        <Link href="/">
+          <MdOutlineArrowBack
+            className="border rounded-full bg-[#F2F2F2]"
+            color="gray"
+            size={45}
+          />
+        </Link>
+      </div>
       <PaymentRadio title="Mandiri" price={120} />
       <PaymentRadio title="Gopay" price={120} />
       <PaymentRadio title="Shopeepay" price={120} />
